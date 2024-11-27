f5e28d18167c11d865c01dfdad9eb3763dae3346
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-11-27 17:45:42 -0300

    Feat(model e controller): iniciando o blog pessoal

---
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,11 @@
+package com.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+
+
+}
