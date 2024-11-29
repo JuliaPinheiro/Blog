05f872802a4f015133ca831c63fc38888bcd780a
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-11-29 19:19:19 -0300

    Feat(controller tema): criação do tema com CRUD, controllerm repository e Model

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,49 @@
+package com.generation.blogpessoal.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 100, message = "A descrição é obrigatória!!")
+    private String descricao;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagens;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Postagem> getPostagens() {
+        return postagens;
+    }
+
+    public void setPostagens(List<Postagem> postagens) {
+        this.postagens = postagens;
+    }
+}
